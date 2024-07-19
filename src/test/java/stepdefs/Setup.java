ad68f2fc6dfda84bbe1e7b3a033f60dcac3a2ce4
owyndwight <owyn.dwight@gymshark.com>
2024-07-19 08:50:54 +0100

    Initial commit

---
+++ src/test/java/stepdefs/Setup.java
@@ -0,0 +1,36 @@
+package stepdefs;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+
+import java.util.logging.Level;
+
+public class Setup {
+
+  public static WebDriver setupAndGetDriver() {
+    return new ChromeDriver(getCapabilities());
+  }
+
+  private static ChromeOptions getCapabilities() {
+    LoggingPreferences logPrefs = new LoggingPreferences();
+    logPrefs.enable(LogType.BROWSER, Level.ALL);
+    logPrefs.enable(LogType.DRIVER, Level.ALL);
+
+    ChromeOptions options = new ChromeOptions();
+
+    options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+    options.addArguments("--enable-automation");
+    options.addArguments("--enable-crash-reporter");
+    options.addArguments("--disable-extensions");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");
+    options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+    options.setAcceptInsecureCerts(true);
+    return options;
+  }
+}
