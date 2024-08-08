85780ec5ad79bc4320812d9cfe267ab5577d8ba7
KodzGS <k.johnson@gymshark.com>
2024-08-08 15:18:19 +0100

    Commit 90 minute test code task added smoke tag and added assert URL and removed line

--- src/test/java/stepdefs/SearchStepDefs.java
+++ src/test/java/stepdefs/SearchStepDefs.java
@@ -5,20 +5,19 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import pageobject.GoogleHomePage;
+
 import java.time.Duration;
-import java.util.List;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchStepDefs {
+
   public WebDriver driver;
 
   @Before
@@ -42,16 +41,14 @@ public class SearchStepDefs {
     driver.manage().window().maximize();
     driver.get("https://www.google.co.uk");
   }
+
   @When("I enter {string} search term")
   public void iEnterSearchTerm(String searchItem) {
 
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     GoogleHomePage.CookiesButton_search(driver).click();
     GoogleHomePage.textBox_search(driver).sendKeys(searchItem);
-    //wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).sendKeys(Keys.ENTER);
     wait.until(ExpectedConditions.elementToBeClickable(GoogleHomePage.GoogleButton_search(driver))).sendKeys(Keys.ENTER);
-    GoogleHomePage.GoogleButton_search(driver).click();
-
   }
 
   @When("I enter {string} in lucky search term")
@@ -60,24 +57,24 @@ public class SearchStepDefs {
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     GoogleHomePage.CookiesButton_search(driver).click();
     GoogleHomePage.textBox_search(driver).sendKeys(searchItem);
-    //wait.until(ExpectedConditions.elementToBeClickable(By.name("btnI"))).sendKeys(Keys.ENTER);//
     wait.until(ExpectedConditions.elementToBeClickable(GoogleHomePage.LuckyButton_search(driver))).sendKeys(Keys.ENTER);
     GoogleHomePage.LuckyButton_search(driver).click();
   }
 
   @Then("results relevant to the search term are returned")
   public void resultsRelevantToTheSearchTermAreReturned() {
-   List<WebElement> resultHeaders = driver.findElements(By.xpath("//a/h3"));
-   for (WebElement header : resultHeaders) {
-     assertThat(header.getText()).as("Search results contains search term").contains("BBC");
-    }}
 
+    // Then the browser should redirect to a URL containing "BBC"
+    assertThat(driver.getCurrentUrl()).as("...").contains("BBC");
+  }
 
   @Then("results to the lucky click are returned")
   public void resultsToTheLuckyClickAreReturned() {
-    List<WebElement> resultHeaders = driver.findElements(By.xpath("//a/h3"));
-    for (WebElement header : resultHeaders) {
-      assertThat(header.getText()).as("Search results contains search term").contains("BBC");
+
+    // Then the browser should redirect to a URL containing "BBC"
+    assertThat(driver.getCurrentUrl()).as("...").contains("bbc");
+
     }
   }
-}
+
+
--- src/test/resources/LuckySearch.feature
+++ src/test/resources/LuckySearch.feature
@@ -3,6 +3,7 @@ Feature: Search
   I want to lucky search the google
   So that results show based on this click
 
+  @Smoke
   Scenario: Clicking lucky search to obtain results
     Given  I am on the Google UK
     When I enter "BBC" in lucky search term
