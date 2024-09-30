d8c2cb67c4c61e0ef79dfe4043072eacbed36da0
akshathashivananjappa <akshatha.shivananjappa@gymshark.com>
2024-09-30 21:24:01 +0100

    Completed all task

---
+++ src/test/java/pageobjects/SearchPage.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+
+public class SearchPage {
+  public static final By ACCEPT_ALL = By.id("L2AGLb");
+  public static final By SEARCH_BUTTON = By.id("APjFqb");
+  public static final By FEELING_LUCKY = By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[4]/div[6]/center/input[2]");
+  public static final By RESULT_HEADERS = By.xpath("//a/h3");
+}
--- src/test/java/stepdefs/SearchStepDefs.java
+++ src/test/java/stepdefs/SearchStepDefs.java
@@ -5,12 +5,11 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageobjects.SearchPage;
 
 import java.time.Duration;
 import java.util.List;
@@ -36,19 +35,40 @@ public class SearchStepDefs {
     driver.get("https://www.google.co.uk");
   }
   
-  @When("I enter a search term")
-  public void iEnterASearchTerm() {
+  @When("I enter a {string}")
+  public void iEnterA(String string) {
+    WebElement acceptAll = driver.findElement(SearchPage.ACCEPT_ALL);
+    acceptAll.sendKeys(Keys.ENTER);
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='L2AGLb2']"))).click();
-    driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("BBC news");
-    driver.findElement(By.xpath("//input[@title='Search']")).sendKeys(Keys.ENTER);
+    WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(SearchPage.SEARCH_BUTTON));
+    searchButton.click();
+  }
+
+  @When("entered a search term and clicked on I am Feeling Lucky")
+  public void enteredASearchTermAndClickedOnIAmFeelingLucky() {
+    WebElement acceptAll = driver.findElement(SearchPage.ACCEPT_ALL);
+    acceptAll.sendKeys(Keys.ENTER);
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(SearchPage.SEARCH_BUTTON));
+    searchButton.click();
+    searchButton.sendKeys("BBC");
+
+    Actions action = new Actions(driver);
+    WebElement luckyButton = driver.findElement(SearchPage.FEELING_LUCKY);
+    action.moveToElement(luckyButton).click().perform();
   }
 
   @Then("results relevant to the search term are returned")
   public void resultsRelevantToTheSearchTermAreReturned() {
-    List<WebElement> resultHeaders = driver.findElements(By.xpath("//a/h3"));
+    List<WebElement> resultHeaders = driver.findElements(SearchPage.RESULT_HEADERS);
     for(WebElement header : resultHeaders) {
       assertThat(header.getText()).as("Search results contains search term").contains("BBC");
     }
+  }
+
+  @Then("navigated to the relavant URL to the search entered")
+  public void navigatedToTheRelavantUrlToTheSearchEntered() {
+    driver.switchTo().newWindow(WindowType.TAB);
+    driver.getCurrentUrl().contains("bbc");
   }
 }
